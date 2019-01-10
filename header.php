b81fec4f026b7fd8a772d6d4cdbb4dd7ec5dc98f
Jillzz93 <45562278+Jillzz93@users.noreply.github.com>
2019-01-10 10:35:31 +0100

    load template

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Grenspark Maas-Swalm en Nette</title>
+
+            <?php //BOOTSTRAP ?>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+            <?php //FONTAWESOME ?>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+            <?php //OWN CSS ?>
+            <link rel="stylesheet" href="/css/style.css" type="text/css">
+
+        </head>
+        <body>
+
+

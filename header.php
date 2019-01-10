cca664a7dd87eb3e18edbfda0210e006a3c5b77f
Jillzz93 <45562278+Jillzz93@users.noreply.github.com>
2019-01-10 16:28:45 +0100

    test

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
         <head>
             <title>Grenspark Maas-Swalm en Nette</title>
 
+            <?php //FONTS ?>
+            <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i|Lora:400,700" rel="stylesheet">
+
             <?php //BOOTSTRAP ?>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
@@ -10,9 +13,48 @@
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
             <?php //OWN CSS ?>
-            <link rel="stylesheet" href="/css/style.css" type="text/css">
+            <link rel="stylesheet" href="/css/stylesheet.css" type="text/css">
 
         </head>
         <body>
+        <nav class="navbar navbar-light navbar-expand-md bg-light fixed-top justify-content-center">
+            <div class="container">
+                <a href="/" class="navbar-brand d-flex w-50 mr-auto"><img src="../img/Logo/NL.svg" alt="MSN-logo" /></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+                    <ul class="navbar-nav w-100 justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">App</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Partners</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">App-store</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <ul class="nav navbar-nav ml-auto w-50 justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NL</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">EN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">DE</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
 
